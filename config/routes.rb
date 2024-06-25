8474fc8876b384679974e1157627504be3c1c2fd
steffi <steffi.amper@gmail.com>
2024-06-25 15:52:59 -0500

    added navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/about'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
   resources :venues
   resources :classifications
   resources :artists
+  get 'about', to: 'pages#about'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
