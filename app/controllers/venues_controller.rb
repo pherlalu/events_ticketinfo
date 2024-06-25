16d21b641c43ce52513a6caa651e5054f360f615
steffi <steffi.amper@gmail.com>
2024-06-25 18:01:50 -0500

    added contents for locations page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,10 @@
 class VenuesController < ApplicationController
   def index
+    @venues = Venue.includes(:events).order('events.date_time DESC').paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @venue = Venue.find(params[:id])
+    @events = @venue.events.order(:date_time).paginate(page: params[:page], per_page: 10)
   end
 end
