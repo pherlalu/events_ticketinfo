595e4385f810c7249b14a1de6f65287c656fe632
steffi <steffi.amper@gmail.com>
2024-06-25 17:23:56 -0500

    added artists content

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.includes(:events).order('events.date_time DESC').paginate(page: params[:page], per_page: 10)
   end
   
   def show
     @artist = Artist.find(params[:id])
+    @events = @artist.events.order(:date_time).paginate(page: params[:page], per_page: 10)
   end
 end
