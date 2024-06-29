b3533716e4bdb05dd4e595902f9e18dcddbcade1
steffi <steffi.amper@gmail.com>
2024-06-28 20:53:44 -0500

    mini magick config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+ 
+  config.active_storage.variant_processor = :mini_magick #THIS IS THE IMPORTANT LINE YOU NEED TO ADD
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
